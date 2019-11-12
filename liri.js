b5bc38482f115acd0150570ebd1ef1062994244e
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2019-11-11 22:03:07 -0500

    added axios stuff

--- liri.js
+++ liri.js
@@ -1,4 +1,46 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+const Spotify = require('node-spotify-api');
+const axios = require('axios');
+
+
+let spotify = new Spotify(keys.spotify);
+
+spotify
+    .search({ type: 'artist', query: 'U2' })
+    .then(function(response) {
+    console.log(response);
+    })
+    .catch(function(err) {
+    console.log(err);
+    });
+
+axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=trilogy')
+    .then(function (response) {
+    // handle success
+    console.log(response);
+    })
+    .catch(function (error) {
+    // handle error
+    console.log(error);
+    })
+    .finally(function () {
+    // always executed
+    });
+
+axios.get('https://rest.bandsintown.com/artists/U2?app_id=codingbootcamp')
+    .then(function (response) {
+    // handle success
+    console.log(response);
+    })
+    .catch(function (error) {
+    // handle error
+    console.log(error);
+    })
+    .finally(function () {
+    // always executed
+    });
+
+
+    
