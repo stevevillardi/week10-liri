32a6b4254c880c7c7d43a7fc3ac039f90269d877
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2019-11-13 21:01:26 -0500

    finished version

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
-console.log('this is loaded');
+console.log('Loaded keys..');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.modules = {
+    omdb: process.env.OMDB_API,
+    bandsintown: process.env.BANDS_API
+  };
--- liri.js
+++ liri.js
@@ -1,46 +1,131 @@
 require("dotenv").config();
 
-const keys = require("./keys.js");
+const keys = require('./keys.js');
+const fs = require('fs')
 const Spotify = require('node-spotify-api');
 const axios = require('axios');
 
-
 let spotify = new Spotify(keys.spotify);
 
-spotify
-    .search({ type: 'artist', query: 'U2' })
-    .then(function(response) {
-    console.log(response);
-    })
-    .catch(function(err) {
-    console.log(err);
+function randomLIRI(){
+    fs.readFile("random.txt", "utf8", (err, data) => {
+      if (err) throw err;
+      //console.log(data);
+      spotifyLIRI("track",data);
     });
+}
 
-axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=trilogy')
-    .then(function (response) {
-    // handle success
-    console.log(response);
-    })
-    .catch(function (error) {
-    // handle error
-    console.log(error);
-    })
-    .finally(function () {
-    // always executed
-    });
+function spotifyLIRI(type,query){
+    spotify
+      .search({ type: type, query: query })
+      .then(function(response) {
+        //console.log(response.tracks.items[0]);
+        let song = response.tracks.items[0]
+        console.log(`==========================`)
+        console.log(`Artist Name: ${song.artists[0].name}`)
+        console.log(`Song Name: ${song.name}`)
+        console.log(`Album Name: ${song.album.name} (${song.album.total_tracks} tracks)`)
+        console.log(`Album Release Date: ${song.album.release_date}`)
+        console.log(`Spotify Preview URL: ${song.external_urls.spotify}`)
+        console.log(`==========================`)
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+}
 
-axios.get('https://rest.bandsintown.com/artists/U2?app_id=codingbootcamp')
-    .then(function (response) {
-    // handle success
-    console.log(response);
-    })
-    .catch(function (error) {
-    // handle error
-    console.log(error);
-    })
-    .finally(function () {
-    // always executed
-    });
+function omdbLIRI(query){
+    axios
+      .get(`http://www.omdbapi.com/?t=${query}&apikey=${keys.modules.omdb}`)
+      .then(function(response) {
+        // handle success
+        //console.log(response.data);
+        let movie = response.data
+        console.log(`==========================`)
+        console.log(`Title: ${movie.Title}`);
+        console.log(`Rated: ${movie.Rated}`);
+        console.log(`Release Date: ${movie.Released}`);
+        console.log(`Runtime: ${movie.Runtime}`);
+        console.log(`Genre: ${movie.Genre}`);
+        console.log(`Language: ${movie.Language}`);
+        console.log(`Country: ${movie.Country}`);
+        console.log(`Actors: ${movie.Actors}`);
+        console.log(`IMDB Rating: ${movie.Ratings[0].Value}`);
+        console.log(`Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`);
+        console.log(`Plot: ${movie.Plot}`);
+        console.log(`==========================`)
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+}
 
+function bandsintownLIRI(query){
+    axios
+      .get(`https://rest.bandsintown.com/artists/${query}/events?app_id=${keys.modules.bandsintown}`)
+      .then(function(response) {
+        // handle success
+        console.log(response.data);
+        let concerts = response.data
+        for(i=0;i<concerts.length;i++){
+            console.log(`Band/Artist Lineup: ${concerts[i].lineup}`)
+            console.log(`Concert Venue: ${concerts[i].venue.name}`)
+            console.log(`Date/Time: ${concerts[i].datetime}`)
+            console.log(`==========================`)
+        }
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+}
 
-    
+//Logic to perform search
+if(!process.argv[2]){
+    console.log("You must enter an action for liri to perform, available actions are:");
+    console.log("   1) spotify <song name>")
+    console.log("   2) concert <artist>")
+    console.log("   3) movie <movie title>")
+    console.log("   4) random")
+}
+else {
+    let searchTerm = process.argv[2].toLowerCase().trim()
+    if(!process.argv[3] && searchTerm !="random"){
+        console.log(`You must enter a search term for liri to a search on ${searchTerm}...`)
+    }
+    else{
+        let searchQuery = ""
+        if(process.argv[3]){searchQuery = process.argv[3].toLowerCase().trim()}
+        switch(searchTerm){
+            case "spotify":
+            spotifyLIRI("track",searchQuery);
+            break;
+
+            case "concert":
+            bandsintownLIRI(searchQuery);
+            break;
+
+            case "movie":
+            omdbLIRI(searchQuery);
+            break;
+
+            case "random":
+            randomLIRI();
+            break;
+
+            default:
+                console.log("Search term entered was invalid, available actions are:")
+                console.log("   1) spotify")
+                console.log("   2) concert")
+                console.log("   3) movie")
+                console.log("   4) random")
+        }
+    }
+}
